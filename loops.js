123bc7471011501069021956493a87bcb7e8be00
SAMRIDHI <53181511+samridhi18@users.noreply.github.com>
2020-09-26 12:53:53 +0530

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,13 @@
+const vowel = [ 'a', 'e', 'i', 'o', 'u' ];
+
+function vowelsAndConsonants(s) {
+    let { vowels, consonants } = (s.split('') || []).reduce((target, item) => {
+        target[vowel.includes(item) ? 'vowels' : 'consonants'].push(item)
+
+        return target;
+    }, { vowels: [], consonants: [] });
+
+    vowels.concat(consonants).forEach((item) => {
+        console.log(item);
+    });
+}
