21df43ac9cc6a9f01cb9f6735570ef9066218b7c
SAMRIDHI <53181511+samridhi18@users.noreply.github.com>
2020-09-26 12:36:40 +0530

    Create AithmeticOperator.js

---
+++ AithmeticOperator.js
@@ -0,0 +1,16 @@
+function getArea(length, width) {
+    let area;
+   
+    area = length * width;
+
+    return area;
+}
+
+
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter = 2 * (length + width);
+    
+    return perimeter;
+}
